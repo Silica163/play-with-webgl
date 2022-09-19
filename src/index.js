74e5ac1d47eab316a3d83938accd26f5f003e71e
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-09-19 20:58:00 +0700

    add gl-matrix libary

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ const canvas = document.getElementById("webgl");
 const gl = canvas.getContext("webgl");
 
 console.log(gl);
+
+// clear canvas color
+// red green blue alpha
+gl.clearColor(0,0,0,1);
+gl.clear(gl.COLOR_BUFFER_BIT);
